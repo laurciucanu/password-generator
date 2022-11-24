5fb7ce9769026871973d8705f66d73f4f06d6676
Laur Ciucanu <laur.ciucanu95@yahoo.com>
2022-11-24 11:03:35 +0200

    second commit

--- src/main/java/com/javatpoint/controller/HelloWorldController.java
+++ src/main/java/com/javatpoint/controller/HelloWorldController.java
@@ -1,62 +1,63 @@
 package com.javatpoint.controller;
+
 import org.passay.CharacterData;
 import org.passay.CharacterRule;
 import org.passay.EnglishCharacterData;
 import org.passay.PasswordGenerator;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
 
 import static org.passay.DigestDictionaryRule.ERROR_CODE;
 
-@RestController
+@Controller
 public class HelloWorldController {
-        public String generatePassayPassword() {
-            PasswordGenerator gen = new PasswordGenerator();
-            CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
-            CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
-            lowerCaseRule.setNumberOfCharacters(2);
+    public String generatePassayPassword(int passwordLength) {
+        PasswordGenerator gen = new PasswordGenerator();
+        CharacterData lowerCaseChars = EnglishCharacterData.LowerCase;
+        CharacterRule lowerCaseRule = new CharacterRule(lowerCaseChars);
+        lowerCaseRule.setNumberOfCharacters(2);
 
-            CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
-            CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
-            upperCaseRule.setNumberOfCharacters(2);
+        CharacterData upperCaseChars = EnglishCharacterData.UpperCase;
+        CharacterRule upperCaseRule = new CharacterRule(upperCaseChars);
+        upperCaseRule.setNumberOfCharacters(2);
 
-            CharacterData digitChars = EnglishCharacterData.Digit;
-            CharacterRule digitRule = new CharacterRule(digitChars);
-            digitRule.setNumberOfCharacters(2);
+        CharacterData digitChars = EnglishCharacterData.Digit;
+        CharacterRule digitRule = new CharacterRule(digitChars);
+        digitRule.setNumberOfCharacters(2);
 
-            CharacterData specialChars = new CharacterData() {
-                public String getErrorCode() {
-                    return ERROR_CODE;
-                }
+        CharacterData specialChars = new CharacterData() {
+            public String getErrorCode() {
+                return ERROR_CODE;
+            }
 
-                public String getCharacters() {
-                    return "!@#$%^&*()_+[]-/.,<>";
-                }
-            };
-            CharacterRule splCharRule = new CharacterRule(specialChars);
-            splCharRule.setNumberOfCharacters(2);
+            public String getCharacters() {
+                return "!@#$%^&*()_+[]-/.,<>";
+            }
+        };
+        CharacterRule splCharRule = new CharacterRule(specialChars);
+        splCharRule.setNumberOfCharacters(2);
 
-            String password = gen.generatePassword(15, splCharRule, lowerCaseRule,
-                    upperCaseRule, digitRule);
-            return password;
-        }
-
-        @RequestMapping("/")
-        public String hello() {
-            return "Hello world!";
-        }
-
-        @RequestMapping("/generate")
-        public String generate() {
-            return "<style>\n" +
-                    ".center {\n" +
-                    "position: absolute;\n" +
-                    "  top: 50%; right: 50%;\n" +
-                    "  transform: translate(50%,-50%);  " +
-                    "}\n" +
-                    "</style>" +
-                    "<h1 class='center'>" + generatePassayPassword() + "</h1> ";
-        }
-
+        String password = gen.generatePassword(passwordLength, splCharRule, lowerCaseRule,
+                upperCaseRule, digitRule);
+        return password;
     }
+
+    @RequestMapping("/")
+    public String index()
+    {
+        return "index";
+    }
+
+    @RequestMapping(value="/save", method=RequestMethod.POST)
+    public ModelAndView save(@ModelAttribute LoginForm form)
+    {
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("password");
+        modelAndView.addObject("generatePassayPassword", generatePassayPassword(form.passwordLength));
+        return modelAndView;
+    }
+}
 
---
+++ src/main/java/com/javatpoint/controller/LoginForm.java
@@ -0,0 +1,13 @@
+package com.javatpoint.controller;
+
+public class LoginForm {
+    public Integer getPasswordLength() {
+        return passwordLength;
+    }
+
+    public void setPasswordLength(Integer passwordLength) {
+        this.passwordLength = passwordLength;
+    }
+
+    Integer passwordLength;
+}
